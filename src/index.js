c2a4ea8c1bbdef1eab2e230619952884e70977cc
BitSar <nick.sarros@gmail.com>
2021-07-04 11:37:57 +1000

    tasks apis created

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+// Users apis 
 app.post('/users', (req, res) => {
     const user = new User(req.body)
 
@@ -21,6 +22,29 @@ app.post('/users', (req, res) => {
 
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+
+    User.findById(req.params.id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+// Tasks apis
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -28,6 +52,28 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+
+    Task.findById(req.params.id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+
+    }).catch((error) => {
+        res.status(500).send(error)
     })
 })
 
