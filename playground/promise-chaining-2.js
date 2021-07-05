5eac632fe610879d9eac011f9f66989267dcec72
BitSar <nick.sarros@gmail.com>
2021-07-05 18:04:40 +1000

    refactor for aysnc/await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,25 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('60dd6052fb481a0b5816aade').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
+// 60dfe100219be417862ab5d0
+
+// Task.findByIdAndDelete('60dd6052fb481a0b5816aade').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('60dfe0c1219be417862ab5cd').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('60dfdd0358a80b16a3c48c07', { age: 1} ).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
-    console.log(result)
-}).catch((error) => {
-    console.log(error)
+// User.findByIdAndUpdate('60dfdd0358a80b16a3c48c07', { age: 1} ).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('60dfdd3b7d58bb16badc21ae', 2).then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
 })
