4c722c2c7ebc0b5f899d8acc6ed0249b2f37c3d7
BitSar <nick.sarros@gmail.com>
2021-07-11 16:08:11 +1000

    Enforce user creation unique on email. Implement login endpoint with validation

--- src/models/user.js
+++ src/models/user.js
@@ -12,6 +12,7 @@ const userSchema= new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(value) {
@@ -42,6 +43,22 @@ const userSchema= new mongoose.Schema({
         }
     }
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
 
 // Enforce password hashing before saving user model
 userSchema.pre('save', async function (next) {
