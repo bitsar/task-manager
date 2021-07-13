498bdd3499c035791cec6b1df8cb975f82f2884a
BitSar <nick.sarros@gmail.com>
2021-07-13 17:52:03 +1000

    Force logout all sessions implemented. Surpressed sensitive user data in response

--- src/index.js
+++ src/index.js
@@ -32,15 +32,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port: ', port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisisaseriesofcharacters', { expiresIn: '7 days' })
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisisaseriesofcharacters')
-    console.log(data)
-}
-
-myFunction()
